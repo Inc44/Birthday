aa3b4f32a671e57704fe44999d7fc9e6d4cdab4b
Inc44 <incognita44@outlook.com>
2025-10-26 23:16:07 +0100

    ocaml formatted, multithreading

--- birthday.ml
+++ birthday.ml
@@ -1,13 +1,29 @@
 (* ocamlopt -I +unix -thread unix.cmxa threads.cmxa -o birthday birthday.ml && ./birthday *)
 open Thread
 open Unix
+
 let days_in_year = 365
 let num_threads = 768
 let people = 24
 let total_simulations = 1_000_000
 let multiplier = 1664525
 let increment = 1013904223
-let simulations_per_thread = total_simulations / num_threads
+
+let get_cpu_count () =
+	try
+		let input_channel = Unix.open_process_in "getconf _NPROCESSORS_ONLN" in
+		let stdout = try input_line input_channel with _ -> "1" in
+		ignore (Unix.close_process_in input_channel);
+		let cpus = try int_of_string (String.trim stdout) with _ -> 1 in
+		max 1 cpus
+	with _ -> 1
+
+let threads =
+	let cpus = get_cpu_count () in
+	min cpus num_threads
+
+let simulations_per_thread = total_simulations / threads
+
 let simulate thread_id data_success_count =
 	let seed = Int64.of_float (Unix.gettimeofday () *. 1_000_000_000.) in
 	let state = ref (Int64.to_int (Int64.logxor seed (Int64.of_int thread_id))) in
@@ -26,13 +42,19 @@ let simulate thread_id data_success_count =
 		if !exactly_two_count = 1 then incr success_count
 	done;
 	data_success_count.(thread_id) <- !success_count
+
 let () =
 	let start = gettimeofday () in
-	let data_success_count = Array.make num_threads 0 in
-	let threads = Array.init num_threads (fun t -> Thread.create (fun id -> simulate id data_success_count) t) in
+	let data_success_count = Array.make threads 0 in
+	let threads =
+		Array.init threads (fun t ->
+				Thread.create (fun id -> simulate id data_success_count) t)
+	in
 	Array.iter Thread.join threads;
 	let total_success_count = Array.fold_left ( + ) 0 data_success_count in
-	let probability = float_of_int total_success_count /. float_of_int total_simulations in
+	let probability =
+		float_of_int total_success_count /. float_of_int total_simulations
+	in
 	Printf.printf "Probability: %.9f\n" probability;
 	let elapsed = gettimeofday () -. start in
 	Printf.printf "Execution Time: %.3f s\n" elapsed
